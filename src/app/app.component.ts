3bfd37774d747d41fe4034211a4daabbd547788d
Blessing <bntoko@gmail.com>
2020-11-27 14:14:24 +0200

    basic setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as THREE from 'three';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'threejsCameras';
+export class AppComponent implements OnInit{
+
+  public scene = new THREE.Scene();
+  public renderer = new THREE.WebGLRenderer();
+
+  ngOnInit() {
+    this.init();
+  }
+
+  private init() {
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(this.renderer.domElement);
+  }
 }
